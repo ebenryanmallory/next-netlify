d4279f0ccfb1ab20d08ddef5166ce9338a2aff98
Eben Mallory <ebenryanmallory@gmail.com>
2021-01-27 11:56:01 -0700

    hello added

--- pages/page.js
+++ pages/page.js
@@ -1,4 +1,6 @@
-function Page({ data }) {
+import Head from 'next/head'
+
+function Page({ userId, id, title, completed }) {
     return (
         <div className="container">
           <Head>
@@ -8,11 +10,13 @@ function Page({ data }) {
     
           <main>
             <p className="description">
-                {data}
+                {completed}
                 <br />
-                {data.userId}                <br />
-                {data.id}                <br />
-                {data.title}
+                {userId}                
+                <br />
+                {id}                
+                <br />
+                {title}
             </p>
           </main>
     
@@ -25,9 +29,9 @@ function Page({ data }) {
     // Fetch data from external API
     const res = await fetch(`https://jsonplaceholder.typicode.com/todos/1`)
     const data = await res.json()
-  
+    const { userId, id, title, completed } = data;
     // Pass data to the page via props
-    return { props: { data } }
+    return { props: { userId, id, title, completed } }
   }
   
   export default Page
